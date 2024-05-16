d5572e7f6aa5c3e99d5cb60d44a47c77863c0ba6
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2024-05-16 21:55:39 +0530

    feat: form submit

--- app/(route)/signup/page.tsx
+++ app/(route)/signup/page.tsx
@@ -4,71 +4,62 @@ import PasswordInput from "@/app/components/PasswordButton";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 function Signup() {
-  const [loginData, setLoginData] = useState({ email: "", password: "" });
   const [signupData, setSignupData] = useState({
     email: "",
     password: "",
   });
 
-  const handleLoginChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLoginData({ ...loginData, [e.target.name]: e.target.value });
-  };
-
   const handleSignupChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSignupData({ ...signupData, [e.target.name]: e.target.value });
   };
 
-  const handleLoginSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Login Data:", loginData);
-    // Add your login logic here
-  };
-
   const handleSignupSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
     console.log("Signup Data:", signupData);
+    router.push("signup/step1");
     // Add your signup logic here
   };
-
+  const router = useRouter();
   return (
     <div className="flex items-center">
       <div className="w-1/2 mr-8">
-        <form
-          className="shadow-md rounded px-8 pt-6 pb-8 mb-4 mx-8"
-          onSubmit={handleLoginSubmit}
-        >
+        <div className="shadow-md rounded px-8 pt-6 pb-8 mb-4 mx-8">
           <h2 className="text-3xl font-bold mb-8">SignUp</h2>
 
-          <div className="mb-8">
-            <label
-              className="block text-gray-400 text-sm font-bold mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
-              type="email"
-              placeholder="Email"
-              name="email"
-              value={loginData.email}
-              onChange={handleLoginChange}
-            />
-          </div>
+          <form onSubmit={handleSignupSubmit}>
+            <div className="mb-8">
+              <label
+                className="block text-gray-400 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={signupData.email}
+                onChange={handleSignupChange}
+                required
+              />
+            </div>
 
-          <div className="flex items-center justify-between">
-            <Link href="signup/step1">
+            <div className="flex items-center justify-between">
               <button
                 className="bg-[#FF9292] hover:bg-[#FFB4B4] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 type="submit"
               >
                 Next
               </button>
-            </Link>
-          </div>
+            </div>
+          </form>
+
           <div className="mt-4">
             Already have an account?{" "}
             <div className="relative inline-block">
@@ -80,7 +71,7 @@ function Signup() {
               </Link>
             </div>
           </div>
-        </form>
+        </div>
       </div>
       <Image
         src="/bg-signup.png"
